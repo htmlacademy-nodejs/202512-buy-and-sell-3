29cd9a810a2db6b7441072f86031682970ddf46f
Panova Natalia <pannat.job@gmail.com>
2021-03-12 21:57:58 +0300

    fix

--- src/express/index.js
+++ src/express/index.js
@@ -8,10 +8,9 @@ const DEFAULT_PORT = 8080;
 
 const app = express();
 
-app.use(`/offers/`, offersRoutes);
-app.use(`/my/`, myRoutes);
+app.use(`/offers`, offersRoutes);
+app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
 
-app.get(`/`, (req, res) => res.send(`Hello, Express.js`));
 app.listen(DEFAULT_PORT,
     () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
